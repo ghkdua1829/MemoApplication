029cd0c844b0014ebcf58d1d791de7ab5d6c8891
KCY <ghkdua059@gmail.com>
2020-02-21 13:41:45 +0900

    gradle 추가 및 메모 데이터클래스 작성

---
+++ app/src/main/java/com/example/memoapplication/MemoAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.memoapplication
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MemoAdapter() : RecyclerView.Adapter<MemoAdapter.MemoViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MemoViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: MemoViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    class MemoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+}
---
+++ app/src/main/java/com/example/memoapplication/data/model/Memo.kt
@@ -0,0 +1,9 @@
+package com.example.memoapplication.data.model
+
+import java.util.*
+
+data class Memo(
+    val title: String,
+    val description: String,
+    val date: Date
+)
