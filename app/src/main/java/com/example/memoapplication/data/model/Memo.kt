9c1518488d3816651629503118f7746c52352842
KCY <ghkdua059@gmail.com>
2020-02-21 16:00:03 +0900

    날짜 타입 임의 수정(데이터 확인 차)

--- app/src/main/java/com/example/memoapplication/data/model/Memo.kt
+++ app/src/main/java/com/example/memoapplication/data/model/Memo.kt
@@ -1,9 +1,7 @@
 package com.example.memoapplication.data.model
 
-import java.util.*
-
 data class Memo(
     val title: String,
     val description: String,
-    val date: Date
+    val date: String
 )
