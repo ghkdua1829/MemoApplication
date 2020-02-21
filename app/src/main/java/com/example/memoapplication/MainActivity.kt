6b16878cb159c04f161d96e29058f4af4864aaf8
KCY <ghkdua059@gmail.com>
2020-02-21 16:00:52 +0900

    MainActivity databinding 적용

--- app/src/main/java/com/example/memoapplication/MainActivity.kt
+++ app/src/main/java/com/example/memoapplication/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.example.memoapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.memoapplication.data.model.Memo
+import com.example.memoapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    val list: List<Memo> = (listOf(
+        Memo(
+            title = "ASda", description = "asdsa", date = "asd"
+        ), Memo(title = "ASda", description = "asdsa", date = "asd")
+    ))
+    private lateinit var memoAdapter: MemoAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this@MainActivity, R.layout.activity_main)
+
+        memoAdapter = MemoAdapter()
+        memoAdapter.setMemoData(list)
+        binding.rvMemo.adapter = memoAdapter
+
     }
 }
